78551fe9c73a4ac2c248a629c9cfed1cdd5b78f5
22002222 <22002222@myrp.edu.sg>
2023-07-04 16:45:17 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22002222/songapp/MainActivity.java
@@ -0,0 +1,100 @@
+package sg.edu.rp.c346.id22002222.songapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+
+import java.util.ArrayList;
+
+import kotlinx.coroutines.scheduling.Task;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etTitle, etSinger, etYear;
+
+    RadioButton rb1, rb2, rb3, rb4, rb5;
+
+    Button btnInsert, btnShowList;
+
+    ArrayList<Song> al;
+    ArrayAdapter<String> aa;
+    ListView lv;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etTitle = findViewById(R.id.etTitle);
+        etSinger = findViewById(R.id.etSinger);
+        etYear = findViewById(R.id.etYear);
+        rb1 = findViewById(R.id.rb1);
+        rb2 = findViewById(R.id.rb2);
+        rb3 = findViewById(R.id.rb3);
+        rb4 = findViewById(R.id.rb4);
+        rb5 = findViewById(R.id.rb5);
+        btnInsert = findViewById(R.id.btnInsert);
+        btnShowList = findViewById(R.id.btnShowList);
+        lv = findViewById(R.id.lv);
+
+
+
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDB db = new myDB(MainActivity.this);
+
+                String title = etTitle.getText().toString();
+                String singer = etSinger.getText().toString();
+                int year = Integer.parseInt(etYear.getText().toString());
+                int stars = 0;
+
+                if(rb1.isChecked()) {
+
+                    stars = 1;
+                }
+                else if(rb2.isChecked()){
+                    stars = 2;
+                }
+
+
+                else if(rb3.isChecked()){
+                    stars = 3;
+
+                }else if(rb4.isChecked()){
+                    stars = 4;
+//
+                }
+                else if(rb5.isChecked()){
+                    stars = 5;
+
+                }
+
+                db.insertSong(title,singer, year, stars);
+
+
+//
+
+            }
+        });
+
+        btnShowList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDB db= new myDB(MainActivity.this);
+                al = db.getSongs();
+
+                aa = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, al);
+                lv.setAdapter(aa);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/sg/edu/rp/c346/id22002222/songapp/myDB.java
@@ -0,0 +1,101 @@
+package sg.edu.rp.c346.id22002222.songapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+import kotlinx.coroutines.scheduling.Task;
+
+public class myDB extends SQLiteOpenHelper {
+    // Start version with 1
+    // increment by 1 whenever db schema changes.
+    private static final int DATABASE_VER = 1;
+    // Filename of the database
+    private static final String DATABASE_NAME = "song.db";
+
+    private static final String TABLE_SONG = "song";
+    private static final String COLUMN_ID = "_id";
+
+    private static final String COLUMN_TITLE= "title";
+    private static final String COLUMN_SINGERS = "singers";
+    private static final String COLUMN_YEAR = "year";
+    private static final String COLUMN_STARS = "star";
+
+
+    public myDB(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VER);
+    }
+
+//    public myDB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+//        super(context, name, factory, version);
+//    }
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTableSql = "CREATE TABLE " + TABLE_SONG + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_TITLE + " TEXT,"+ COLUMN_SINGERS + " TEXT,"+ COLUMN_YEAR+ " INTEGER,"
+                + COLUMN_STARS + " INTEGER )";
+        db.execSQL(createTableSql);
+        Log.i("info", "created tables");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void insertSong(String title, String singers, int year, int stars){
+
+        // Get an instance of the database for writing
+        SQLiteDatabase db = this.getWritableDatabase();
+        // We use ContentValues object to store the values for
+        //  the db operation
+        ContentValues values = new ContentValues();
+
+        values.put(COLUMN_TITLE, title);
+
+        values.put(COLUMN_SINGERS, singers);
+
+        values.put(COLUMN_YEAR, year);
+
+        values.put(COLUMN_STARS, stars);
+
+        db.insert(TABLE_SONG, null, values);
+
+        db.close();
+    }
+
+    public ArrayList<Song> getSongs() {
+        ArrayList<Song> songs = new ArrayList<Song>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns = {COLUMN_ID, COLUMN_TITLE, COLUMN_SINGERS, COLUMN_YEAR, COLUMN_STARS};
+        Cursor cursor = db.query(TABLE_SONG, columns, null, null, null, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                int id  = cursor.getInt(0);
+                String title = cursor.getString(1);
+                String singers = cursor.getString(2);
+                int year = cursor.getInt(3);
+                int stars = cursor.getInt(4);
+
+                Song obj = new Song(id, title, singers, year, stars);
+                songs.add(obj);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return songs;
+    }
+}
